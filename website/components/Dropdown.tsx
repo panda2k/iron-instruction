dd08c3050836572f351be8a9da3d9ad017e8fe91
panda2k <thelowkeypanda@gmail.com>
2022-09-05 11:36:39 -0700

    Make update profile form pretty

--- website/components/Dropdown.tsx
+++ website/components/Dropdown.tsx
@@ -30,7 +30,7 @@ const Dropdown: NextPage<Props> = (props: Props) => {
     return (
         <div className="relative inline-block text-left">
             <div>
-                <button onClick={handleClick} type="button" className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                <button onClick={handleClick} type="button" className="inline-flex w-full justify-center rounded-md border border-black bg-white px-4 py-2 text-sm font-medium text-black shadow-sm hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
                     {props.heading}
                     <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
@@ -41,14 +41,14 @@ const Dropdown: NextPage<Props> = (props: Props) => {
                 <div className="py-1" role="none">
                     {props.menuItems.map(item => {
                         return (
-                            <a href={item.href} className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} >{item.name}</a>
+                            <a href={item.href} className="text-black block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} >{item.name}</a>
                         )
                     })}
                 </div>
                 {props.logoutButton && (
                     <div className="py-1">
                         <form onSubmit={handleLogout}>
-                            <button type="submit" className="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabIndex={-1}>Sign out</button>
+                            <button type="submit" className="text-black block w-full px-4 py-2 text-left text-sm" role="menuitem" tabIndex={-1}>Sign out</button>
                         </form>
                     </div>
                 )}
--- website/pages/dashboard.tsx
+++ website/pages/dashboard.tsx
@@ -5,14 +5,13 @@ import { useUserContext } from "../context/UserContext";
 import Api from '../utils/api'
 import { Athlete, User, UserType } from "../utils/api.types";
 
-const Profile: NextPage = () => {
+const Dashboard: NextPage = () => {
     const [loading, setLoading] = useState<boolean>(true)
     const { user, setUser } = useUserContext()
     const [userForm, setUserForm] = useState<User>(user as User)
     const [editing, setEditing] = useState<boolean>(false)
 
     useEffect(() => {
-        setLoading(true)
         const fetchData = async () => {
             try {
                 const fetchedUser: User = await Api.getUserInfo()
@@ -22,12 +21,26 @@ const Profile: NextPage = () => {
                 window.location.href = "/login"
             }
             setLoading(false)
+
         }
 
         fetchData()
     }, [])
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // style form
+    useEffect(() => {
+        if (!loading && user?.userType == UserType.ATHLETE) {
+            const targetWidth: number = (document.querySelector("#reference") as HTMLDivElement).offsetWidth
+
+            document.querySelectorAll(".whitespace-nowrap:not(#reference)").forEach((element) => {
+                const inputWidth: number = targetWidth - element.querySelector("label")!.offsetWidth
+                const input = element.querySelector("input") as HTMLElement
+                input.style.width = `${inputWidth}px`
+            })
+        }
+    }, [loading, editing])
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         setUserForm({ ...userForm, [e.target.name]: e.target.value })
     }
 
@@ -68,79 +81,82 @@ const Profile: NextPage = () => {
             {
                 !loading && (
                     <div className="flex flex-row items-center">
-                        <form id="profile-form" className="ml-10 flex flex-col outline-2 rounded-sm mt-8">
+                        <div className="ml-10 outline-2 rounded-md mt-8 ring-black ring-1 ring-opacity-30 py-3 px-5">
                             <h1 className="underline text-center text-3xl mb-3">Profile</h1>
-                            <div className="whitespace-nowrap">
-                                <label htmlFor="name">Name: </label>
-                                <input onChange={handleChange} className="enabled:border-black enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-sm focus:outline-none" type="text" name="name" value={userForm.name} disabled={!editing} id="name" />
-                            </div>
-                            <div className="whitespace-nowrap">
-                                <label htmlFor="email">Email: </label>
-                                <input onChange={handleChange} className="enabled:border-black enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-sm focus:outline-none" type="email" name="email" value={userForm.email} disabled={!editing} id="email" />
-                            </div>
-                            {
-                                userForm!.userType == UserType.ATHLETE &&
-                                (
-                                    <div>
-                                        <div className="whitespace-nowrap">
-                                            <label htmlFor="dob">Date of Birth: </label>
-                                            <input className="enabled:border-black enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-sm focus:outline-none" onChange={handleChange} required={true} type="date" name="dob" value={(userForm as Athlete).dob} disabled={!editing} id="dob" />
+                            <form id="profile-form" className="[&>*]:w-fit">
+                                <div className="whitespace-nowrap">
+                                    <label htmlFor="name">Name: </label>
+                                    <input onChange={handleChange} className="enabled:border-black shadow-sm enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-md focus:outline-none" type="text" name="name" value={userForm.name} disabled={!editing} id="name" />
+                                </div>
+                                <div className="whitespace-nowrap">
+                                    <label htmlFor="email">Email: </label>
+                                    <input onChange={handleChange} className="enabled:border-black shadow-sm enabled:pl-1  enabled:mb-1.5 enabled:border enabled:rounded-md focus:outline-none" type="email" name="email" value={userForm.email} disabled={!editing} id="email" />
+                                </div>
+                                {
+                                    userForm!.userType == UserType.ATHLETE &&
+                                    (
+                                        <div className="[&>*]:w-fit">
+                                            <div className="whitespace-nowrap">
+                                                <label htmlFor="dob">Date of Birth: </label>
+                                                <input className="enabled:border-black shadow-sm enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-md focus:outline-none" onChange={handleChange} required={true} type="date" name="dob" value={(userForm as Athlete).dob} disabled={!editing} id="dob" />
+                                            </div>
+                                            <div className="whitespace-nowrap">
+                                                <label htmlFor="weight">Weight (kg): </label>
+                                                <input className="enabled:border-black shadow-sm enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-md focus:outline-none" onChange={handleChange} type="number" name="weight" value={(userForm as Athlete).weight} disabled={!editing} id="weight" />
+                                            </div>
+                                            <div className="whitespace-nowrap" id="reference">
+                                                <label htmlFor="weightClass">Weight Class: </label>
+                                                <select className="enabled:border-black shadow-sm enabled:pl-1 enabled:mb-1.5 enabled:border enabled:py-0.5 enabled:rounded-md focus:outline-none" onChange={handleChange} name="weightClass" id="weightClass" value={(userForm as Athlete).weightClass} disabled={!editing}>
+                                                    <option disabled={true} selected={true}>No weight class saved</option>
+                                                    <option value={"44kg / 97lbs"}>44kg / 97lbs</option>
+                                                    <option value={"48kg / 105.7lbs"}>48kg / 105.7lbs</option>
+                                                    <option value={"52kg / 114.5lbs"}>52kg/ 114.5lbs</option>
+                                                    <option value={"56kg / 123.5lbs"}>56kg / 123.5lbs</option>
+                                                    <option value={"60kg / 132.2lbs"}>60kg / 132.2lbs</option>
+                                                    <option value={"67.5kg / 148.7lbs"}>67.5kg/ 148.7lbs</option>
+                                                    <option value={"75kg / 165.2lbs"}>75kg / 165.2lbs</option>
+                                                    <option value={"82.5kg / 181.7lbs"}>82.5kg / 181.7lbs</option>
+                                                    <option value={"90kg / 198.2lbs"}>90kg / 198.2lbs</option>
+                                                    <option value={"100kg / 220lbs"}>100kg / 220lbs</option>
+                                                    <option value={"100+kg / 220+lbs"}>100+kg / 220+lbs</option>
+                                                    <option value={"110kg / 242lbs"}>110kg / 242lbs</option>
+                                                    <option value={"125kg / 275lbs"}>125kg / 275lbs</option>
+                                                    <option value={"140kg / 308.5lbs"}>140kg / 308.5lbs</option>
+                                                    <option value={"140+kg / 308.5+lbs"}>140+kg / 308.5+lbs</option>
+                                                </select>
+                                            </div>
+                                            <div className="whitespace-nowrap">
+                                                <label htmlFor="weight">Height (in): </label>
+                                                <input className="enabled:border-black shadow-sm enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-md focus:outline-none" onChange={handleChange} type="number" name="height" value={(userForm as Athlete).height} disabled={!editing} id="height" />
+                                            </div>
+                                            <div className="whitespace-nowrap">
+                                                <label htmlFor="squatMax">Squat Max (kg): </label>
+                                                <input className="enabled:border-black shadow-sm enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-md focus:outline-none" onChange={handleChange} type="number" name="squatMax" value={(userForm as Athlete).squatMax} disabled={!editing} id="squatMax" />
+                                            </div>
+                                            <div className="whitespace-nowrap">
+                                                <label htmlFor="benchMax">Bench Max (kg): </label>
+                                                <input className="enabled:border-black shadow-sm enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-md focus:outline-none" onChange={handleChange} type="number" name="benchMax" value={(userForm as Athlete).benchMax} disabled={!editing} id="benchMax" />
+                                            </div>
+                                            <div className="whitespace-nowrap">
+                                                <label htmlFor="deadliftMax">Deadlift Max (kg): </label>
+                                                <input className="enabled:border-black shadow-sm enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-md focus:outline-none" onChange={handleChange} type="number" name="deadliftMax" value={(userForm as Athlete).deadliftMax} disabled={!editing} id="deadliftMax" />
+                                            </div>
                                         </div>
-                                        <div className="whitespace-nowrap">
-                                            <label htmlFor="weight">Weight (kg): </label>
-                                            <input className="enabled:border-black enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-sm focus:outline-none" onChange={handleChange} type="number" name="weight" value={(userForm as Athlete).weight} disabled={!editing} id="weight" />
-                                        </div>
-                                        <div className="whitespace-nowrap">
-                                            <label htmlFor="weightClass">Weight Class: </label>
-                                            <select className="enabled:border-black enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-sm focus:outline-none" name="weightClass" id="weightClass" value={(userForm as Athlete).weightClass} disabled={!editing}>
-                                                <option disabled={true} selected={true}>No weight class saved</option>
-                                                <option value={"44kg / 97lbs"}>44kg / 97lbs</option>
-                                                <option value={"48kg / 105.7lbs"}>48kg / 105.7lbs</option>
-                                                <option value={"52kg / 114.5lbs"}>52kg/ 114.5lbs</option>
-                                                <option value={"56kg / 123.5lbs"}>56kg / 123.5lbs</option>
-                                                <option value={"60kg / 132.2lbs"}>60kg / 132.2lbs</option>
-                                                <option value={"67.5kg / 148.7lbs"}>67.5kg/ 148.7lbs</option>
-                                                <option value={"75kg / 165.2lbs"}>75kg / 165.2lbs</option>
-                                                <option value={"82.5kg / 181.7lbs"}>82.5kg / 181.7lbs</option>
-                                                <option value={"90kg / 198.2lbs"}>90kg / 198.2lbs</option>
-                                                <option value={"100kg / 220lbs"}>100kg / 220lbs</option>
-                                                <option value={"100+kg / 220+lbs"}>100+kg / 220+lbs</option>
-                                                <option value={"110kg / 242lbs"}>110kg / 242lbs</option>
-                                                <option value={"125kg / 275lbs"}>125kg / 275lbs</option>
-                                                <option value={"140kg / 308.5lbs"}>140kg / 308.5lbs</option>
-                                                <option value={"140+kg / 308.5+lbs"}>140+kg / 308.5+lbs</option>
-                                            </select>
-                                        </div>
-                                        <div className="whitespace-nowrap">
-                                            <label htmlFor="weight">Height (in): </label>
-                                            <input className="enabled:border-black enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-sm focus:outline-none" onChange={handleChange} type="number" name="height" value={(userForm as Athlete).height} disabled={!editing} id="height" />
-                                        </div>
-                                        <div className="whitespace-nowrap">
-                                            <label htmlFor="squatMax">Squat Max (kg): </label>
-                                            <input className="enabled:border-black enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-sm focus:outline-none" onChange={handleChange} type="number" name="squatMax" value={(userForm as Athlete).squatMax} disabled={!editing} id="squatMax" />
-                                        </div>
-                                        <div className="whitespace-nowrap">
-                                            <label htmlFor="benchMax">Bench Max (kg): </label>
-                                            <input className="enabled:border-black enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-sm focus:outline-none" onChange={handleChange} type="number" name="benchMax" value={(userForm as Athlete).benchMax} disabled={!editing} id="benchMax" />
-                                        </div>
-                                        <div className="whitespace-nowrap">
-                                            <label htmlFor="deadliftMax">Deadlift Max (kg): </label>
-                                            <input className="enabled:border-black enabled:pl-1 enabled:mb-1.5 enabled:border enabled:rounded-sm focus:outline-none" onChange={handleChange} type="number" name="deadliftMax" value={(userForm as Athlete).deadliftMax} disabled={!editing} id="deadliftMax" />
-                                        </div>
+                                    )
+                                }
+                                {editing ?
+                                    <div className="flex flex-row justify-center mt-3">
+                                        <button className="mr-2 purple-bg rounded-md px-6 py-1 text-white" type="button" onClick={cancelEdit}>Cancel</button>
+                                        <button className="purple-bg rounded-md px-6 py-1 text-white" type="button" onClick={saveProfile}>Save</button>
                                     </div>
-                                )
-                            }
-                            {editing ?
-                                <div className="flex flex-row justify-center mt-3">
-                                    <button className="mr-2 purple-bg rounded-md px-6 py-1 text-white" type="button" onClick={cancelEdit}>Cancel</button>
-                                    <button className="purple-bg rounded-md px-6 py-1 text-white" type="button" onClick={saveProfile}>Save</button>
-                                </div>
-                                :
-                                <div className="flex flex-row justify-center mt-3">
-                                    <button className="purple-bg rounded-md px-6 py-1 text-white" type="button" onClick={toggleEdit}>Edit</button>
-                                </div>
-                            }
-                        </form>
+                                    :
+                                    <div className="flex flex-row justify-center mt-3">
+                                        <button className="purple-bg rounded-md px-6 py-1 text-white" type="button" onClick={toggleEdit}>Edit</button>
+                                    </div>
+                                }
+                            </form>
+
+                        </div>
                     </div>
                 )
             }
@@ -149,5 +165,5 @@ const Profile: NextPage = () => {
     )
 }
 
-export default Profile
+export default Dashboard
 
